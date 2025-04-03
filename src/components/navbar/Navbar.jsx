7f7c41e3bd1a3fe809b9bd42f73eee220c40f6e7
kenjayev <kenjayevazizbek2oo4@gmail.com>
2025-04-03 20:18:08 +0500

    End of responsive menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,24 @@
 import "./navbar.css";
 import { useTranslation } from "react-i18next";
 import { IoMenu } from "react-icons/io5";
+import { IoCloseSharp } from "react-icons/io5";
 import ChangeLang from "../change-lang/ChangeLang";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
   const { t, i18n } = useTranslation();
-  const changeLanguage = () => {
-    i18n.changeLanguage("eng");
+  const openMenu = () => {
+    setIsOpenMenu(true);
+    document.body.style.overflow = "hidden";
+  };
+  const closeMenu = () => {
+    setIsOpenMenu(false);
+    document.body.style.overflow = "auto";
   };
 
   return (
-    <div className="navbar">
+    <div className={`navbar ${isOpenMenu ? "openMenu" : ""}`}>
       <div className="container">
         <div className="navbar__wrapper">
           <div className="navbar__logo-box">
@@ -21,33 +29,40 @@ export default function Navbar() {
 
           <nav className="navbar__menu">
             <ul className="navbar__menu-list">
-              <li className="navbar__menu-list">
+              <li className="navbar__menu-item">
                 <a href="#" className="navbar__menu-link">
                   {t("headerSection.navList.link1")}
                 </a>
               </li>
-              <li className="navbar__menu-list">
+              <li className="navbar__menu-item">
                 <a href="#" className="navbar__menu-link">
                   {t("headerSection.navList.link2")}
                 </a>
               </li>
-              <li className="navbar__menu-list">
+              <li className="navbar__menu-item">
                 <a href="#" className="navbar__menu-link">
                   {t("headerSection.navList.link3")}
                 </a>
               </li>
-              <li className="navbar__menu-list">
+              <li className="navbar__menu-item">
                 <a href="#" className="navbar__menu-link">
                   {t("headerSection.navList.link4")}
                 </a>
               </li>
-              <li className="navbar__menu-list">
+              <li className="navbar__menu-item">
                 <ChangeLang />
               </li>
             </ul>
+            <div
+              className="navbar__menu-btn navbar__menu-close"
+              onClick={closeMenu}
+            >
+              <IoCloseSharp className="navbar__menu-icon" />
+            </div>
           </nav>
+          <div className="backdrop" onClick={closeMenu}></div>
 
-          <div className="navbar__menu-btn">
+          <div className="navbar__menu-btn" onClick={openMenu}>
             <IoMenu className="navbar__menu-icon" />
           </div>
         </div>
