e1b43dcfa181e9fd426e2dd91ff4c2239709897a
kenjayev <kenjayevazizbek2oo4@gmail.com>
2025-06-02 17:40:13 +0500

    delate webp imgs

--- src/components/video-content/VideoContent.jsx
+++ src/components/video-content/VideoContent.jsx
@@ -1,6 +1,5 @@
 import "./videoContent.css";
 import { useEffect, useRef } from "react";
-import { FaPlay } from "react-icons/fa";
 
 export default function VideoContent() {
   const videoRef = useRef(null);
@@ -13,22 +12,6 @@ export default function VideoContent() {
 
   return (
     <section className="video__content">
-      {/* <div className="video__link-box">
-        <a href="http://youtube.com" target="_blank">
-          <FaPlay />
-        </a>
-      </div> */}
-      {/* <iframe
-        width="100%"
-        height="100%"
-        src="https://www.youtube.com/embed/BapSQFJPMM0?autoplay=1&mute=1&loop=1&playlist=BapSQFJPMM0&controls=0&modestbranding=1&rel=0"
-        frameborder="0"
-        allow="autoplay; encrypted-media"
-        allowfullscreen
-        loading="lazy"
-        referrerpolicy="no-referrer"
-      ></iframe> */}
-
       <video autoPlay loop muted playsInline className="w-full h-auto">
         <source src="/video/highway-loop.mp4" type="video/mp4" />
       </video>
